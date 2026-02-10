aa0d58fa54cb3d32be72755c1021299a678ea873
Rihan Ahmed <95731297+Rihan444@users.noreply.github.com>
2026-02-10 11:49:50 +0600

    Add files via upload

---
+++ ApkPatcher/Utils/Credits.py
@@ -0,0 +1,28 @@
+from ..ANSI_COLORS import ANSI; C = ANSI()
+
+
+# ---------------- Credits ----------------
+def Credits():
+    exit(f"""
+                     💢  Credit  💢
+
+{C.S} Flutter SSL & TG Patch {C.E}{C.G}BHHC Infinity Tech {C.S}{C.P} BHHC Trips & Tricks {C.E}
+
+{C.S} Pine Hook {C.E}{C.G}BHHC Infinity Tech | 残页 {C.S}{C.P} BHHC Trips & Tricks | @canyie {C.E}
+
+{C.S} MITM {C.E}{C.G} Niklas Higi {C.S}{C.P} https://github.com/Rihan444/apk-mitm {C.E}
+
+{C.S} APKTool {C.E}{C.G} Connor Tumbleson {C.S}{C.P} @iBotPeaches {C.E}
+
+{C.S} APKEditor {C.E}{C.G} REAndroid {C.S}{C.P} @kikfox {C.E}
+
+{C.S} Uber-Apk-Signer {C.E}{C.G} Patrick Favre {C.S}{C.P} @patrickfav {C.E}
+
+{C.S} My Channel {C.E}{C.CC} 🇧🇩 {C.OG} BHHC Tips & Tricks {C.B}{C.G}{C.CC} 🇧🇩 {C.S}{C.P} @bhhctipsandtricks {C.E}
+
+{C.S} CREATOR {C.E}{C.G}{C.S}{C.P} @BHHC Tips & Tricks {C.E}
+
+
+{C.S}  NOTE  {C.E} {C.Y} Please Maintain Our Credits.
+
+    """)
